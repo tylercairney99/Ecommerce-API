931a26870d6153f0ef75220f67b4cbcd099f56dc
Tyler <tylerc99@uw.edy>
2023-12-20 01:25:20 -0800

    Created initial Product model class
    - Defined the Product entity with relevant JPA annotations
    - Included fields for product ID, name, price and stock quantity
    - Provided getter and setter methods for all fields

--- src/main/java/com/example/ecommerceapi/api/model/Product.java
+++ src/main/java/com/example/ecommerceapi/api/model/Product.java
@@ -1,4 +1,60 @@
 package com.example.ecommerceapi.api.model;
 
+import jakarta.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "products")
 public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private Long id;
+
+    private String productName;
+
+    private BigDecimal Price;
+
+    private int StockQuantity;
+
+
+    public Product() {
+        // no arg constructor for JPA
+    }
+
+    public Product(final String theProductName, final BigDecimal thePrice, final int theStockQuantity) {
+        this.productName = theProductName;
+        this.Price = thePrice;
+        this.StockQuantity = theStockQuantity;
+    }
+
+    public Long getID() {
+        return id;
+    }
+
+    public void setID(Long theID) {
+        this.id = theID;
+    }
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String theProductName) {
+        this.productName = theProductName;
+    }
+    public BigDecimal getPrice() {
+        return Price;
+    }
+
+    public void setPrice(BigDecimal thePrice) {
+        this.Price = thePrice;
+    }
+    public int getStockQuantity() {
+        return StockQuantity;
+    }
+
+    public void setStockQuantity(int theStockQuantity) {
+        this.StockQuantity = theStockQuantity;
+    }
 }
