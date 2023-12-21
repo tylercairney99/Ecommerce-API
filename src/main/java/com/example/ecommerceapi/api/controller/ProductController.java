6b2ffa7ac69c9a1c5d9c64a9b69a48c81354c395
Tyler <tylerc99@uw.edy>
2023-12-20 16:12:23 -0800

    Integrate ProductDTO into ProductController for Enhanced Data Handling
    - Modified ProductController to utilize ProductDTO for data transfer in create, read, and update operations.
    - Enabled validation on ProductDTO objects in addProduct and updateProduct methods using @Valid annotation.
    - Utilized ModelMapper for efficient mapping between ProductDTO and Product entity.
    - Updated JavaDoc comments to reflect the implementation and usage of ProductDTO.
    - Aligned CRUD operations in ProductController with the DTO pattern for improved data abstraction and management.

--- src/main/java/com/example/ecommerceapi/api/controller/ProductController.java
+++ src/main/java/com/example/ecommerceapi/api/controller/ProductController.java
@@ -4,10 +4,14 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import com.example.ecommerceapi.service.ProductService;
 import com.example.ecommerceapi.api.model.Product;
+import com.example.ecommerceapi.api.dto.ProductDTO;
 import org.springframework.web.server.ResponseStatusException;
 import org.springframework.http.HttpStatus;
+import org.modelmapper.ModelMapper;
+import jakarta.validation.Valid;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * REST controller for managing products.
@@ -31,60 +35,90 @@ public class ProductController {
     private final ProductService myProductService;
 
     /**
+     * ModelMapper for converting between DTO and entity classes.
+     */
+    private final ModelMapper myModelMapper;
+
+    /**
      * Constructs a ProductController with dependency injection of the ProductService.
      *
      * @param theProductService (The service handling product-related business logic)
      */
     @Autowired // Automatically injects an instance of ProductService
-    public ProductController(final ProductService theProductService) {
+    public ProductController(final ProductService theProductService, final ModelMapper theModelMapper) {
         this.myProductService = theProductService;
+        this.myModelMapper = theModelMapper;
     }
 
     /**
-     * Retrieves a list of all products.
+     * Retrieves all products and returns them as a list of ProductDTOs.
      *
-     * @return A list of Product objects.
+     * This method fetches a list of all Product entities, maps them to ProductDTOs,
+     * and returns the list of DTOs. Useful for providing a view of product data
+     * suitable for client-side use.
+     *
+     * @return A list of ProductDTOs representing all products.
      */
     @GetMapping // Endpoint for getting all products
-    public List<Product> getAllProducts() {
-        return myProductService.getAllProducts();
+    public List<ProductDTO> getAllProducts() {
+        final List<Product> products = myProductService.getAllProducts();
+        return products.stream()
+                .map(product -> myModelMapper.map(product, ProductDTO.class))
+                .collect(Collectors.toList());
     }
 
     /**
-     * Retrieves a specific product by its unique identifier.
+     * Retrieves a specific product by its unique identifier and returns it as a ProductDTO.
      *
-     * @param theProductID (The ID of the product to retrieve)
-     * @return The Product object if found
-     * @throws ResponseStatusException If no product with the given ID is found
+     * This method fetches a product from the database using its ID. If found, the product
+     * is mapped to a ProductDTO and returned. If no product is found with the provided ID,
+     * a ResponseStatusException with HttpStatus.NOT_FOUND is thrown.
+     *
+     * @param theProductID (The unique identifier of the product to be retrieved)
+     * @return A ProductDTO representing the retrieved product.
+     * @throws ResponseStatusException If no product with the given ID is found.
      */
     @GetMapping("/{theProductID}") // Endpoint to get a product by its ID
-    public Product getProductByID(@PathVariable final Long theProductID) {
-        return myProductService.getProductByID(theProductID)
+    public ProductDTO getProductByID(@PathVariable final Long theProductID) {
+        final Product product = myProductService.getProductByID(theProductID)
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Product with ID " + theProductID + " not found"));
+        return myModelMapper.map(product, ProductDTO.class);
     }
 
     /**
-     * Creates a new product.
+     * Creates a new product from the provided ProductDTO.
      *
-     * @param theProduct (The Product object to be created)
-     * @return The newly created Product object
+     * Accepts a ProductDTO object with product details, validates it, maps it to a Product entity,
+     * and persists it in the database. The newly created product is then mapped back to a ProductDTO
+     * and returned. This method generates an HTTP 201 status code upon the successful creation of a product.
+     *
+     * @param theProductDTO (The DTO containing the new product's details)
+     * @return The ProductDTO of the newly created product.
      */
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED) // Endpoint to create a new product, returns HTTP 201 status on creation
-    public Product addProduct(@RequestBody final Product theProduct) {
-        return myProductService.addProduct(theProduct);
+    public ProductDTO addProduct(@Valid @RequestBody final ProductDTO theProductDTO) {
+        final Product product = myModelMapper.map(theProductDTO, Product.class);
+        final Product createdProduct = myProductService.addProduct(product);
+        return myModelMapper.map(createdProduct, ProductDTO.class);
     }
 
     /**
-     * Updates an existing product's information.
+     * Updates an existing product's details based on the provided ProductDTO.
      *
-     * @param theProductID (The ID of the product to update)
-     * @param theProductDetails (The Product object containing updated information)
-     * @return The updated Product object
+     * This method accepts a ProductDTO object containing updated product information, along with the product's ID.
+     * The ProductDTO is validated and then mapped to a Product entity, which is used to update the existing product
+     * in the database. The updated product is then mapped back to a ProductDTO and returned.
+     *
+     * @param theProductID (The ID of the product to be updated)
+     * @param theProductDTO (The DTO containing the updated details for the product)
+     * @return The ProductDTO representing the updated product.
      */
-    @PutMapping("/{theProductID}") // Endpoint to update a product by its ID
-    public Product updateProduct(@PathVariable final Long theProductID, @RequestBody final Product theProductDetails) {
-        return myProductService.updateProduct(theProductID, theProductDetails);
+    @PutMapping("/{theProductID}")
+    public ProductDTO updateProduct(@PathVariable Long theProductID, @Valid @RequestBody final ProductDTO theProductDTO) {
+        final Product productToUpdate = myModelMapper.map(theProductDTO, Product.class);
+        final Product updatedProduct = myProductService.updateProduct(theProductID, productToUpdate);
+        return myModelMapper.map(updatedProduct, ProductDTO.class);
     }
 
     /**
