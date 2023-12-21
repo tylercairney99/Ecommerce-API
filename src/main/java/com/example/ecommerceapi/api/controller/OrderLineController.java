1fd40281db0b1d2692fc3c170332c54fe4fd13c7
Tyler <tylerc99@uw.edy>
2023-12-21 09:23:48 -0800

    Integrate OrderLineDTO into OrderLineController for Enhanced Data Handling
    - Modified OrderLineController to utilize OrderLineDTO for data transfer in create, read, and update operations.
    - Enabled validation on OrderLineDTO objects in addOrderLine and updateOrderLine methods using @Valid annotation.
    - Utilized ModelMapper for efficient mapping between OrderLineDTO and OrderLine entity.
    - Updated JavaDoc comments to reflect the implementation and usage of OrderLineDTO.
    - Aligned CRUD operations in OrderLineController with the DTO pattern for improved data abstraction and management.

--- src/main/java/com/example/ecommerceapi/api/controller/OrderLineController.java
+++ src/main/java/com/example/ecommerceapi/api/controller/OrderLineController.java
@@ -1,13 +1,18 @@
 package com.example.ecommerceapi.api.controller;
 
+import com.example.ecommerceapi.api.dto.OrderDTO;
+import com.example.ecommerceapi.api.dto.OrderLineDTO;
 import com.example.ecommerceapi.api.model.OrderLine;
 import com.example.ecommerceapi.service.OrderLineService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
+import jakarta.validation.Valid;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * REST controller for managing order lines.
@@ -29,60 +34,93 @@ public class OrderLineController {
     private final OrderLineService myOrderLineService;
 
     /**
+     * ModelMapper for converting between DTO and entity classes.
+     */
+    private final ModelMapper myModelMapper;
+
+    /**
      * Constructs an OrderLineController with dependency injection of the OrderLineService.
      *
      * @param theOrderLineService (The service handling order line-related business logic)
+     * @param theModelMapper (Mapping between DTO and entity classes)
      */
     @Autowired // Automatically injects an instance of OrderLineService
-    public OrderLineController(final OrderLineService theOrderLineService) {
+    public OrderLineController(final OrderLineService theOrderLineService, final ModelMapper theModelMapper) {
         this.myOrderLineService = theOrderLineService;
+        this.myModelMapper = theModelMapper;
     }
 
     /**
-     * Retrieves all order lines.
+     * Retrieves all order lines and returns them as a list of OrderLineDTOs.
      *
-     * @return A list of all OrderLine objects.
+     * This method fetches a list of all order lines, maps them to OrderLineDTOs,
+     * and returns the list of DTOs. Useful for providing a view of order line data
+     * suitable for client-side use.
+     *
+     * @return A list of OrderLineDTOs representing all order lines.
      */
     @GetMapping // Endpoint for getting all order lines
-    public List<OrderLine> getAllOrderLines() {
-        return myOrderLineService.getAllOrderLines();
+    public List<OrderLineDTO> getAllOrderLines() {
+        final List<OrderLine> orderLines = myOrderLineService.getAllOrderLines();
+        return orderLines.stream()
+                .map(orderLine -> myModelMapper.map(orderLine, OrderLineDTO.class))
+                .collect(Collectors.toList());
     }
 
     /**
-     * Retrieves a specific order line by its unique identifier.
+     * Retrieves a specific order line by its unique identifier and returns it as an OrderLineDTO.
      *
-     * @param theOrderLineID (The ID of the order line to retrieve)
-     * @return The OrderLine object if found.
+     * This method fetches an order line from the database using its ID. If found, the order line
+     * is mapped to an OrderLineDTO and returned. If no order line is found with the provided ID,
+     * a ResponseStatusException with HttpStatus.NOT_FOUND is thrown.
+     *
+     * @param theOrderLineID (The unique identifier of the order line to be retrieved)
+     * @return An OrderLineDTO representing the retrieved order line.
      * @throws ResponseStatusException If no order line with the given ID is found.
      */
     @GetMapping("/{theOrderLineID}") // Endpoint to get an order line by its ID
-    public OrderLine gerOrderLineByID(@PathVariable final Long theOrderLineID) {
-        return myOrderLineService.getOrderLineByID(theOrderLineID)
+    public OrderLineDTO gerOrderLineByID(@PathVariable final Long theOrderLineID) {
+        final OrderLine orderLine = myOrderLineService.getOrderLineByID(theOrderLineID)
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "OrderLine with ID " + theOrderLineID + " not found"));
+        return myModelMapper.map(orderLine, OrderLineDTO.class);
     }
 
     /**
-     * Creates a new order line.
+     * Creates a new order line and returns it as an OrderLineDTO.
      *
-     * @param theOrderLine (The OrderLine object to be created)
-     * @return The newly created OrderLine object.
+     * This method takes an OrderDTO as input, maps it to an OrderLine entity,
+     * and adds the new order line to the database. The newly created order line
+     * is then mapped to an OrderLineDTO and returned. It returns an HTTP 201 status
+     * upon successful creation.
+     *
+     * @param theOrderLineDTO (The OrderDTO representing the new order line to be created)
+     * @return An OrderLineDTO representing the newly created order line.
      */
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED) // Endpoint to create a new order line, returns HTTP 201 status on creation
-    public OrderLine addOrderLine(@RequestBody final OrderLine theOrderLine) {
-        return myOrderLineService.addOrderLine((theOrderLine));
+    public OrderLineDTO addOrderLine(@Valid @RequestBody final OrderDTO theOrderLineDTO) {
+        final OrderLine orderLine = myModelMapper.map(theOrderLineDTO, OrderLine.class);
+        final OrderLine createdOrderLine = myOrderLineService.addOrderLine(orderLine);
+        return myModelMapper.map(createdOrderLine, OrderLineDTO.class);
     }
 
     /**
-     * Updates an existing order line's details.
+     * Updates an order line by its unique identifier and returns the updated OrderLineDTO.
      *
-     * @param theOrderLineID (The ID of the order line to update)
-     * @param theOrderLineDetails (The OrderLine object containing updated details)
-     * @return The updated OrderLine object.
+     * This method takes the unique identifier of the order line to be updated and an updated
+     * OrderLineDTO as input. It maps the updated DTO to an OrderLine entity and performs
+     * the update operation in the database. The method returns the updated order line as an
+     * OrderLineDTO.
+     *
+     * @param theOrderLineID (The unique identifier of the order line to be updated)
+     * @param theOrderLineDTO (The updated OrderLineDTO containing the new data)
+     * @return An OrderLineDTO representing the updated order line.
      */
     @PutMapping("/{theOrderLineID}") // Endpoint to update an order line by its ID
-    public OrderLine updateOrderLine(@PathVariable final Long theOrderLineID, @RequestBody final OrderLine theOrderLineDetails) {
-        return myOrderLineService.updateOrderLine(theOrderLineID, theOrderLineDetails);
+    public OrderLineDTO updateOrderLine(@PathVariable final Long theOrderLineID, @Valid @RequestBody final OrderLineDTO theOrderLineDTO) {
+        final OrderLine orderLineToUpdate = myModelMapper.map(theOrderLineDTO, OrderLine.class);
+        final OrderLine updatedOrderLine = myOrderLineService.updateOrderLine(theOrderLineID, orderLineToUpdate);
+        return myModelMapper.map(updatedOrderLine, OrderLineDTO.class);
     }
 
     /**
