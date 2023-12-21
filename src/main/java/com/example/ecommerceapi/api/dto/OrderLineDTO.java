a11686d8fbafb06d01cb91a59f3dabc5d35af41d
Tyler <tylerc99@uw.edy>
2023-12-21 05:50:58 -0800

    Refactored 'OrderLineDTO'
    - Changed naming conventions

--- src/main/java/com/example/ecommerceapi/api/dto/OrderLineDTO.java
+++ src/main/java/com/example/ecommerceapi/api/dto/OrderLineDTO.java
@@ -20,55 +20,55 @@ public class OrderLineDTO {
      * The unique identifier of the product in the order line.
      */
     @NotNull(message = "Product ID is required")
-    private Long productId;
+    private Long myProductID;
 
     /**
      * The quantity of the product in the order line.
      */
     @Min(value = 1, message = "Quantity must be at least 1")
-    private int quantity;
+    private int myQuantity;
 
     /**
      * The price of the product per unit.
      */
     @NotNull(message = "Unit price is required")
-    private BigDecimal unitPrice;
+    private BigDecimal myUnitPrice;
 
     /**
      * The total price for this order line item.
      */
-    private BigDecimal lineTotal;
+    private BigDecimal myLineTotal;
 
     // Default constructor
     public OrderLineDTO() {
     }
 
     // All-arguments constructor
-    public OrderLineDTO(Long productId, int quantity, BigDecimal unitPrice) {
-        this.productId = productId;
-        this.quantity = quantity;
-        this.unitPrice = unitPrice;
-        this.lineTotal = unitPrice.multiply(new BigDecimal(quantity));
+    public OrderLineDTO(final Long theProductId, final int theQuantity, final BigDecimal theUnitPrice) {
+        this.myProductID = theProductId;
+        this.myQuantity = theQuantity;
+        this.myUnitPrice = theUnitPrice;
+        this.myLineTotal = theUnitPrice.multiply(new BigDecimal(theQuantity));
     }
 
     // Getters and Setters with JavaDoc comments
 
     /**
      * Gets the product ID of the order line.
-     *
+     *zc
      * @return the product ID.
      */
     public Long getProductId() {
-        return productId;
+        return myProductID;
     }
 
     /**
      * Sets the product ID of the order line.
      *
-     * @param productId the new product ID.
+     * @param theProductId the new product ID.
      */
-    public void setProductId(Long productId) {
-        this.productId = productId;
+    public void setProductId(final Long theProductId) {
+        this.myProductID = theProductId;
     }
 
     /**
@@ -77,19 +77,19 @@ public class OrderLineDTO {
      * @return the quantity.
      */
     public int getQuantity() {
-        return quantity;
+        return myQuantity;
     }
 
     /**
      * Sets the quantity of the product in the order line.
      *
-     * @param quantity the new quantity.
+     * @param theQuantity the new quantity.
      */
-    public void setQuantity(int quantity) {
-        this.quantity = quantity;
+    public void setQuantity(final int theQuantity) {
+        this.myQuantity = theQuantity;
         // Recalculate line total if quantity is set after initialization
-        if (this.unitPrice != null) {
-            this.lineTotal = this.unitPrice.multiply(new BigDecimal(quantity));
+        if (this.myUnitPrice != null) {
+            this.myLineTotal = this.myUnitPrice.multiply(new BigDecimal(theQuantity));
         }
     }
 
@@ -99,18 +99,18 @@ public class OrderLineDTO {
      * @return the unit price.
      */
     public BigDecimal getUnitPrice() {
-        return unitPrice;
+        return myUnitPrice;
     }
 
     /**
      * Sets the unit price of the product in the order line.
      *
-     * @param unitPrice the new unit price.
+     * @param theUnitPrice the new unit price.
      */
-    public void setUnitPrice(BigDecimal unitPrice) {
-        this.unitPrice = unitPrice;
+    public void setUnitPrice(final BigDecimal theUnitPrice) {
+        this.myUnitPrice = theUnitPrice;
         // Recalculate line total if unit price is set after initialization
-        this.lineTotal = unitPrice.multiply(new BigDecimal(this.quantity));
+        this.myLineTotal = theUnitPrice.multiply(new BigDecimal(this.myQuantity));
     }
 
     /**
@@ -119,7 +119,7 @@ public class OrderLineDTO {
      * @return the line total.
      */
     public BigDecimal getLineTotal() {
-        return lineTotal;
+        return myLineTotal;
     }
 
     // Note: Typically, there's no setter for lineTotal as it's derived from quantity and unit price
